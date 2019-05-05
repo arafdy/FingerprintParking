8ef8aa86a5a1c5ddae3504b7a943fa6744b44fe6
rafdy amestira <arafdy@ymail.com>
2019-05-05 18:48:03 +0700

    added servo module for parking gate

--- fingerprints_mega_v1.0/fingerprints_mega_v1.0.ino
+++ fingerprints_mega_v1.0/fingerprints_mega_v1.0.ino
@@ -1,17 +1,5 @@
 /***************************************************
-  This is an example sketch for our optical Fingerprint sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout
-  ----> http://www.adafruit.com/products/751
-
-  These displays use TTL Serial to communicate, 2 pins are required to
-  interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
+this project use 2 fingerprint module with arduino mega
  ****************************************************/
 #include <SPI.h>
 #include <SD.h>
